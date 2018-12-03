8b26a66ddd331d2528a18f843750687d75734f00
Денис Аминев <daminev@skbkontur.ru>
2018-12-03 15:09:56 +0500

    Complete first day

---
+++ AdventOfCode2018/DayFirst.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace AdventOfCode2018
+{
+    class DayFirst
+    {
+        public (int, List<int>) ParseInput(string fileAddress)
+        {
+            StreamReader file = new StreamReader(fileAddress);
+            var count = 0;
+            var list = new List<int>();
+            while (true)
+            {
+                var line = file.ReadLine();
+                if (line == null) break;
+                count = count + Convert.ToInt32(line);
+                list.Add(count);
+            }
+
+            return (count, list);
+        }
+
+        public int ParseInputSecond(string fileAddress)
+        {
+            var items = ParseInput(fileAddress);
+            var counter = items.Item1;
+            while (true)
+            {
+                var file2 = new StreamReader(fileAddress);
+                while (true)
+                {
+                    var line = file2.ReadLine();
+                    if (line == null) break;
+                    counter = counter + Convert.ToInt32(line);
+
+                    foreach (var record in items.Item2)
+                    {
+                        if (counter == record)
+                        {
+                            return counter;
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
